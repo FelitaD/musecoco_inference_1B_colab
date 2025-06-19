724f2fe35615ae3e469d6d4e4648ae26adb991a4
Felita <donorfelita@msn.com>
2025-06-19 16:13:52 +0000

    Add updated inference script with content root

--- 2-attribute2music_model/interactive_1billion.sh
+++ 2-attribute2music_model/interactive_1billion.sh
@@ -10,49 +10,43 @@ need_num=2
 temp=1.0
 ngram=0
 
-
 datasets_name="truncated_2560"
 checkpoint_name="checkpoint_2_280000"
 BATCH_SIZE=2
 
+# Remove the backslash - use proper quoting instead
+CONTENT_ROOT="/content/drive/Othercomputers/My Mac/google_colab/musecoco/2-attribute2music_model"
+
 # models parameters
-
-
 device=0
 date="0505"
 mkdir -p "log/${date}/${command_name}"
 model_name="linear_mask-${model_size}"
 
-
-
 # models parameters
-DATA_DIR="../data/$datasets_name"
-
-checkpoint_path="../checkpoints/${model_name}/${checkpoint_name}.pt"
-
-
-ctrl_command_path="../data/infer_input/${command_name}.bin"
-save_root="../generation/${date}/${model_name}-${checkpoint_name}/${command_name}/topk${k}-t${temp}-ngram${ngram}"
-log_root="../log/${date}/${model_name}"
-
-
+DATA_DIR="${CONTENT_ROOT}/data/$datasets_name"
+checkpoint_path="${CONTENT_ROOT}/checkpoints/${model_name}/${checkpoint_name}.pt"
+ctrl_command_path="${CONTENT_ROOT}/data/infer_input/${command_name}.bin"
+save_root="${CONTENT_ROOT}/generation/${date}/${model_name}-${checkpoint_name}/${command_name}/topk${k}-t${temp}-ngram${ngram}"
+log_root="${CONTENT_ROOT}/log/${date}/${model_name}"
 
 export CUDA_VISIBLE_DEVICES=$device
 
 echo "generating from ${checkpoint_path}"
 echo "save to ${save_root}"
 
-cd linear_mask
+cd "${CONTENT_ROOT}/linear_mask"
 
-mkdir -p ${save_root}
-mkdir -p ${log_root}
+# Add quotes around these mkdir commands
+mkdir -p "${save_root}"
+mkdir -p "${log_root}"
 
 python -u interactive_dict_v5_1billion.py \
-${DATA_DIR}/data-bin \
+"${DATA_DIR}/data-bin" \
 --task language_modeling_control \
---path $checkpoint_path \
---ctrl_command_path $ctrl_command_path \
---save_root $save_root \
+--path "$checkpoint_path" \
+--ctrl_command_path "$ctrl_command_path" \
+--save_root "$save_root" \
 --need_num ${need_num} \
 --start ${start} \
 --end ${end} \
@@ -65,5 +59,3 @@ ${DATA_DIR}/data-bin \
 --no-repeat-ngram-size ${ngram} \
 --buffer-size ${BATCH_SIZE} \
 --batch-size ${BATCH_SIZE}   #> ${log_root}/${command_name}-s${start}-e${end}.log 2>&1
-
-
