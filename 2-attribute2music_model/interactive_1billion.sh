afa4e71fc32bb0f6f7c358dce124334c163213a6
Felita <donorfelita@msn.com>
2025-06-19 21:09:13 +0200

    Update inference script with correct paths

--- 2-attribute2music_model/interactive_1billion.sh
+++ 2-attribute2music_model/interactive_1billion.sh
@@ -2,7 +2,6 @@
 start=${1}
 end=${2}
 #input args
-PARENT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 model_size="1billion"
 k=15
 command_name="infer_test"
@@ -25,24 +24,11 @@ mkdir -p "log/${date}/${command_name}"
 model_name="linear_mask-${model_size}"
 
 # models parameters
-<<<<<<< Updated upstream
 DATA_DIR="${CONTENT_ROOT}/data/$datasets_name"
 checkpoint_path="/root/.cache/huggingface/hub/models--XinXuNLPer--MuseCoco_attribute2music/snapshots/8d9df584e3e09da88601546e7980323221f12644/checkpoint_2_280000.pt"
 ctrl_command_path="${CONTENT_ROOT}/data/infer_input/${command_name}.bin"
 save_root="${CONTENT_ROOT}/generation/${date}/${model_name}-${checkpoint_name}/${command_name}/topk${k}-t${temp}-ngram${ngram}"
 log_root="${CONTENT_ROOT}/log/${date}/${model_name}"
-=======
-DATA_DIR="$PARENT_DIR/data/$datasets_name"
-
-checkpoint_path="$PARENT_DIR/checkpoints/${model_name}/${checkpoint_name}.pt"
-
-
-ctrl_command_path="$PARENT_DIR/data/infer_input/${command_name}.bin"
-save_root="$PARENT_DIR/generation/${date}/${model_name}-${checkpoint_name}/${command_name}/topk${k}-t${temp}-ngram${ngram}"
-log_root="$PARENT_DIR/log/${date}/${model_name}"
-
-
->>>>>>> Stashed changes
 
 export CUDA_VISIBLE_DEVICES=$device
 
